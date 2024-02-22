ff150159fc459d04a6117ed8df63b8393b294fbf
kimoon Hong <johnhong515@gmail.com>
2024-02-22 15:45:03 +0900

    modify

--- main.js
+++ main.js
@@ -1,7 +1,12 @@
 // 뉴스를 갖고 오는 함수.
 const API_KEY = `1c373cb782eb496db82d499781dfdc42`
 let newsList = []
+let url = `https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`
+const menus = document.querySelectorAll(".menus button")
 
+menus.forEach(menu=>menu.addEventListener("click", (event)=>getNewsByCategory(event)))
+
+// 사이드 바
 const openNav = () => {
     document.getElementById("mySidenav").style.width = "250px";
 };
@@ -9,6 +14,8 @@ const openNav = () => {
 const closeNav = () => {
     document.getElementById("mySidenav").style.width = "0";
 };
+
+// 검색창
 
 const openSearchBox = () => {
     let inputArea = document.getElementById("input-area");
@@ -19,14 +26,34 @@ const openSearchBox = () => {
     }
 };
 
-const getLatesNews = async () => {
-    const url = new URL(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${API_KEY}`); 
+// 중복되는거
+const getNews = async () => {
     const response = await fetch(url)
     const data = await response.json()
     newsList = data.articles
     render();
-    console.log("dddd", newsList)
+}
+
+// 검색 키워드
+const getNewsByKeyword = async () => {
+    const keyword = document.getElementById("serach-input").value;
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&q=${keyword}&apiKey=${API_KEY}`);
+    getNews();
+
 };
+
+
+const getLatesNews = async () => {
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&apiKey=${API_KEY}`); 
+    getNews();
+};
+
+const getNewsByCategory = async (event) => {
+    const category = event.target.textContent.toLowerCase();
+    url = new URL(`https://newsapi.org/v2/top-headlines?country=kr&category=${category}&apiKey=${API_KEY}`);
+    
+    getNews();
+}
 
 const render = () => {
     // 무엇을 보여줄 것인가?
@@ -63,3 +90,8 @@ const render = () => {
 }
 
 getLatesNews();
+
+// 1. 버튼들의 클릭 이벤트를 만들어준다.
+// 2. 카테고리별 뉴스 가져오기
+// 3. 그 뉴스를 보여주기.
+
